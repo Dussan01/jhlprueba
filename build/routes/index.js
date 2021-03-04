7032741e479dec219063ecba4de66ecd11d1ca63
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-04 12:28:33 -0500

    index

--- build/routes/index.js
+++ build/routes/index.js
@@ -5,26 +5,26 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/', (req, res) => {
-  res.render('layouts/principal');
+    res.render('layouts/principal');
 });
-router.get('/personero', async (req, res) => {
-  // const datos = await pool.query('select * from candidatos');
-  console.log(datos);
-  res.render('forms/view_personero');
+router.get('/personero', async(req, res) => {
+    // const datos = await pool.query('select * from candidatos');
+    console.log(datos);
+    res.render('forms/forms/view_personero');
 });
 router.post('/personero', (req, res) => {
-  res.render('forms/view_personero');
+    res.render('forms/view_personero');
 });
 router.get('/contralor', (req, res) => {
-  res.render('forms/view_contralor');
+    res.render('forms/view_contralor');
 });
 router.post('/contralor', (req, res) => {
-  res.render('forms/view_contralor');
+    res.render('forms/view_contralor');
 });
 router.get('/representante', (req, res) => {
-  res.render('forms/view_representante');
+    res.render('forms/view_representante');
 });
 router.post('/representante', (req, res) => {
-  res.render('forms/representante');
+    res.render('forms/representante');
 });
 module.exports = router;

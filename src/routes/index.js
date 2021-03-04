d0cc829c9c0f9853265a9b29158fe4ba6b6be3e4
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-04 11:52:48 -0500

    Version Prueba

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+
+
+router.get('/', (req, res) => {
+    res.render('layouts/principal')
+});
+
+router.get('/personero', async(req, res) => {
+    // const datos = await pool.query('select * from candidatos');
+    console.log(datos)
+    res.render('forms/view_personero')
+});
+router.post('/personero', (req, res) => {
+    res.render('forms/view_personero')
+});
+
+router.get('/contralor', (req, res) => {
+    res.render('forms/view_contralor')
+});
+router.post('/contralor', (req, res) => {
+    res.render('forms/view_contralor')
+});
+
+router.get('/representante', (req, res) => {
+    res.render('forms/view_representante')
+});
+router.post('/representante', (req, res) => {
+    res.render('forms/representante')
+});
+
+module.exports = router;
